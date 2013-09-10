a1f1d6932e87ed3c9775084855f678fbf1b6354b
Louis-Alban KIM <louis.alban.kim@gmail.com>
2013-09-10 11:27:23 +0200

    Change left controller width

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,6 +3,7 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.rootViewController = UISplitViewController.new.tap do |splitController|
+      splitController.masterColumnWidth = 500 # Private API
       filtersController = FiltersController.new
       navigationController = UINavigationController.alloc.initWithRootViewController(filtersController)
       imageController = ImageController.new
